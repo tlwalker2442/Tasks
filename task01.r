65b40d1e4db4d1907c038ff6841d699836d40f12
tlwalker2442 <tlw0048@mix.wvu.edu>
2021-01-25 19:12:52 -0500

    <Taylor> Task 01 upload b

--- task01.r
+++ task01.r
@@ -1,1 +1,26 @@
 library(swirl)
+Taylor
+...
+1
+2
+1
+1
+1
+5+7
+x <- 5+7
+x
+y <- x-3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 100
+my_sqrt <- sqrt(z-1)
+1
+3
+my_sqrt
+my_div <- z/my_sqrt
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
